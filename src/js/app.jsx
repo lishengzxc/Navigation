60c7febb085e072707cf1a99321897be00c6e522
lishengzxc <eric@lishengcn.cn>
2015-07-26 13:51:23 +0800

    update

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -5,7 +5,7 @@ var UrlBox = React.createClass({
       urlItems.push(<UrlItem key={i}/>)
     }
     return (
-      <div><ul className="n-main">{urlItems}</ul><ToTop/></div>
+      <div><ul className="n-main">{urlItems}</ul><ToTop/><AddUrl/></div>
     );
   }
 });
@@ -16,7 +16,7 @@ var UrlItem = React.createClass({
       <li className="n-main-item">
         <a href="http://www.baidu.com" target="_blank">
           <img src="https://ss0.bdstatic.com/l4oZeXSm1A5BphGlnYG/icon/93364.png" alt=""/>
-        baidu</a>
+        Google</a>
       </li>
     );
   }
@@ -46,6 +46,21 @@ var ToTop = React.createClass({
   render: function () {
     return (
       <button className="totop"><i className="fa fa-arrow-up"></i></button>
+    )
+  }
+});
+
+var AddUrl = React.createClass({
+  render: function () {
+    return (
+      <div className="pop">
+        <form action="">
+          <div><input className="n-inputbox " type="text" placeholder="书签名"/></div>
+          <div><input className="n-inputbox " type="text" placeholder="书签地址"/></div>
+          <div><input className="n-inputbox " type="text" placeholder="书签ICON"/></div>
+          <button type="submit">添加</button>
+        </form>
+      </div>
     )
   }
 });
